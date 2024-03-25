43db23c7d965671761485125b17ad37491fbe45a
tahmedofficial <tanvirofcuo@gmail.com>
2024-03-25 14:39:52 +0600

    finish job details section

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>Oops!!!</h2>
-            <Link className="py-1 px-4 bg-[#151515] text-white rounded-3xl hover:opacity-30 duration-300" to="/">Go Back</Link>
+        <div className="flex flex-col justify-center items-center mt-36">
+            <h2 className="text-7xl text-red-700 mb-16">Oops!!!</h2>
+            <Link className="py-3 px-16 bg-[#151515] text-2xl text-white rounded-full hover:opacity-30 duration-300" to="/">Go Back</Link>
         </div>
     );
 };
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,7 +1,13 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { SlLocationPin } from "react-icons/sl";
+import { AiOutlineDollar } from "react-icons/ai";
+import { MdOutlineMail } from "react-icons/md";
+import { IoCalendarOutline } from "react-icons/io5";
+import { FiPhone } from "react-icons/fi";
 import { saveJobApplication } from "../../utility/localStorage";
+import bg from "../../assets/images/bg1.png";
 
 const JobDetails = () => {
 
@@ -12,6 +18,7 @@ const JobDetails = () => {
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt);
     console.log(id, job);
+    const { job_description, salary, job_title, contact_information, job_responsibility, educational_requirements, experiences } = job;
 
     const handleApplyJob = () => {
         saveJobApplication(idInt)
@@ -20,13 +27,60 @@ const JobDetails = () => {
 
     return (
         <div>
-            <h2>Job Details of: {id}</h2>
-            <div className="grid gap-4 md:grid-cols-4">
-                <div className="border md:col-span-3">
-                    <h2>Details</h2>
+
+            <div className="bg-indigo-50 pt-14 md:relative md:h-72 md:flex">
+                <div className="md:absolute">
+                    <img src={bg} alt="" />
                 </div>
-                <div className="border">
-                    <h2>side bar</h2>
+
+                <div className="flex w-full justify-center items-center">
+                    <h2 className="text-3xl mt-14 mb-10 font-bold md:pl-20">Job Details</h2>
+                </div>
+            </div>
+
+            <div className="grid my-28 gap-4 lg:grid-cols-4 md:w-5/6 mx-auto">
+
+                <div className="lg:col-span-3 px-4">
+                    <h2 className="text-xl font-bold">Job Description:
+                        <span className="text-gray-500 font-normal"> {job_description}</span>
+                    </h2>
+                    <h2 className="my-10 text-xl font-bold">Job Responsibility:
+                        <span className="text-gray-500 font-normal"> {job_responsibility}</span>
+                    </h2>
+                    <h2 className="text-xl font-bold">Educational Requirements:
+                        <span className="text-gray-500 font-normal"> {educational_requirements}</span>
+                    </h2>
+                    <h2 className="my-10 text-xl font-bold">Experiences:
+                        <span className="text-gray-500 font-normal"> {experiences}</span>
+                    </h2>
+                </div>
+
+                <div className="px-4 md:px-0">
+                    <div className="bg-indigo-50 p-6 mb-6 rounded-lg">
+                        <h2 className="border-b pb-4 border-gray-400 text-2xl font-semibold">Job Details</h2>
+                        <h3 className="text-xl mt-6 flex items-center gap-2 text-gray-600 font-semibold">
+                            <span className="text-indigo-400"><AiOutlineDollar /></span>Salary :
+                            <span className="text-gray-400"> {salary}</span>
+                        </h3>
+                        <h3 className="text-xl flex items-center gap-2 mb-8 mt-2 text-gray-600 font-semibold">
+                            <span className="text-indigo-400"><IoCalendarOutline /></span>Job Title :
+                            <span className="text-gray-400"> {job_title}</span>
+                        </h3>
+
+                        <h2 className="border-b pb-4 border-gray-400 text-2xl font-semibold">Contact Information</h2>
+                        <h3 className="text-xl flex items-center gap-2 mt-6 text-gray-600 font-semibold">
+                            <span className="text-indigo-400"><FiPhone /></span>Phone :
+                            <span className="text-gray-400"> {contact_information.phone}</span>
+                        </h3>
+                        <h3 className="text-xl flex items-center gap-2 my-2 text-gray-600 font-semibold">
+                            <span className="text-indigo-400"><MdOutlineMail /></span>Email :
+                            <span className="text-gray-400"> {contact_information.email}</span>
+                        </h3>
+                        <h3 className="text-xl flex gap-2 mb-8 text-gray-600 font-semibold">
+                            <span className="text-indigo-400 mt-2"><SlLocationPin /></span>Address :
+                            <span className="text-gray-400"> {contact_information.address}</span>
+                        </h3>
+                    </div>
                     <button onClick={handleApplyJob} className="btn md:w-full hover:opacity-80 bg-gradient-to-r from-indigo-400 to-indigo-500 text-white">Apply Now</button>
                 </div>
 
